29d4df581ff69b1cd235a5f2cd143f591be480c0
hotsoup42 <jose22gw@gmail.com>
2021-09-15 13:17:53 -0700

    created and added models/Comment.js

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+
+const {Model, DataTypes}=require('sequelize');
+const { now } = require('sequelize/types/lib/utils');
+const sequelize = require = require('../config/connection');
+class Comment extends Model{}
+Comment.init(
+    {
+        content:{
+            type: DataTypes.TEXT
+        },
+        dateCreated:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:DataTypes.NOW
+        }
+    },
+    {
+        sequelize,
+        modelName:'Comment'
+    }
+);
+module.exports = Comment ;
