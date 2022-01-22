47b4ab07111a9879a52efa454f3c96ffcb73ae9d
Hotsoup22 <jose22gw@gmail.com>
2022-01-21 23:46:55 -0800

    cleanded up empty space

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -68,14 +68,12 @@ router.delete('/:id', withAuth, async ( req, res ) => {
 		const blogpostData = await BlogPost.destroy( {
 		where:{
 			id: req.params.id,
-			
 		}
 	});
-
 	if (!blogpostData) {
 		res.status(404).json({ message: 'No blogpostData found with this id!' });
 		return;
-	}  
+	}
 		res.status(200).json(blogpostData);
 	} catch (err) {
 		res.status(500).json(err);
