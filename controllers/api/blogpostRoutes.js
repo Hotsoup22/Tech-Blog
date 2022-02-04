a832c2711b013b66d11765385d51bd866de7a4ea
Hotsoup22 <jose22gw@gmail.com>
2022-02-04 13:27:34 -0800

    refactor, cleaned up code by taking out commented code/unused

--- controllers/api/blogpostRoutes.js
+++ controllers/api/blogpostRoutes.js
@@ -1,33 +1,14 @@
 const router = require('express').Router();
 const { BlogPost, User, Comment } = require( '../../models' );
 const withAuth = require( '../../utils/auth' );
-// GET All Blogpost data with comments and users testing
-router.get( '/', async ( req, res ) => {
-	try {
-		const blogpostData = await BlogPost.findAll( {
-			include: [
-				{ model: User,
-					attributes: ['name'] },
-				{ model: Comment,
-					include: [
-						{ model: User,
-							attributes: ['name'] }
-					] }
-			]
-		} );
 
-		res.json( blogpostData );
-
-	} catch ( err ) {
-		res.status( 500 ).json( err );
-	}
-} );
 // Create new blogpost
 router.post( '/', withAuth, async ( req, res ) => {
 	try {
 		const blogpostData = await BlogPost.create( {
 			...req.body,
-			userId: req.session.user_id
+			userId: req.session.user_id,
+			// date_created: req.session.date_created
 		} );
 
 		res.status( 200 ).json( blogpostData );
@@ -36,32 +17,7 @@ router.post( '/', withAuth, async ( req, res ) => {
 		res.status( 400 ).json( err );
 	}
 } );
-// Display single blogpost with comments
-router.get( '/:id', async ( req, res ) => {
 
-	try {
-
-		const blogpostData = await BlogPost.findByPk( req.params.id, {
-			include: [
-				{ model: User,
-					attributes: ['name']},
-				{ model: Comment,
-					include: [
-						{ model: User,
-							attributes: ['name'] }
-					]}
-			]
-		});
-
-		const blogpost = blogpostData.get( { plain: true } );
-		res.render( 'singleBlogpost', {
-			blogpost,
-			logged_in: req.session.logged_in
-		} );
-	} catch ( err ) {
-		res.status( 500 ).json( err );
-	}
-});
 // delete blogpost
 router.delete('/:id', withAuth, async ( req, res ) => {
 	try {
